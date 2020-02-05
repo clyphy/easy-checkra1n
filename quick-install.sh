c62ccfebf3c62863e9db48fdff5ba5fe51dc6b4c
chris1000 <chris1000@users.noreply.github.com>
2020-02-05 09:56:06 -0600

    Create quick-install.sh

---
+++ quick-install.sh
@@ -0,0 +1,5 @@
+sudo -s
+echo "deb https://assets.checkra.in/debian /" | sudo tee -a /etc/apt/sources.list
+sudo apt-key adv --fetch-keys https://assets.checkra.in/debian/archive.key
+sudo apt update
+apt install checkra1n
